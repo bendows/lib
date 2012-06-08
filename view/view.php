92b4dd19a825fe59b8db2568801cfc3d8d0c5dbd
Ben <ben@insecure.co.za>
2012-06-08 10:22:11 -0700

    indentation test 2

--- view/view.php
+++ view/view.php
@@ -1,105 +1,93 @@
 <?php
 
-class lib_view  extends object {
+class lib_view extends object {
 
-	var $viewfile = false;
-	var $layout = false;
+    var $viewfile = false;
+    var $layout = false;
+    var $input = array();
+    var $viewvars = array();
+    var $autorender = true;
+    var $autolayout = true;
+    var $hasrendered = false;
+    var $output = false;
+    var $pagevars = array('hostname', 'title', 'remoteip', 'here', 'referer', 'viewvars', 'viewfile', 'layout');
+    var $scripts = array();
 
-	var $input = array();
-	var $viewvars = array();
+    function __construct(&$page) {
+        if (!is_object($page))
+            echo "Page[$page] is not a page object";
 
-	var $autorender = true;
-	var $autolayout = true;
+        //Suck in some variables from the page object
+        foreach ($this->pagevars as $var)
+            if (isset($page->{$var}))
+                $this->{$var} = $page->{$var};
 
-	var $hasrendered = false;
-	var $output = false;
-	
-	var $pagevars = array('hostname', 'title', 'remoteip', 'here', 'referer', 'viewvars', 'viewfile', 'layout');
+        if (is_array($page->helpers))
+            if (count($page->helpers))
+                foreach ($page->helpers as $helper) {
+                    $classname = "lib_helper_{$helper}";
+                    $this->{$helper} = & new $classname($this);
+                }
+    }
 
-	var $scripts = array();
+    function addscript($a = '') {
+        if (in_array($a, $this->scripts))
+            return;
+        $this->scripts[] = $a;
+    }
 
-	function __construct(&$page) {
-		if (! is_object($page)) 
-			echo "Page[$page] is not a page object";
+    function flash($key = false) {
+        if (!$key)
+            return;
 
-		//Suck in some variables from the page object
-		foreach ($this->pagevars as $var)
-			if (isset($page->{$var}))
-				$this->{$var} = $page->{$var};
+        if (empty($key))
+            return;
 
-		/*
-		 * $count = count($this->pagevars);
-		for ($j = 0; $j < $count; $j++) {
-			$var = $this->pagevars[$j];
-			$this->{$var} = $page->{$var};
-		}
-	 */
-		if (is_array($page->helpers))
-		if (count($page->helpers))
-		foreach ($page->helpers as $helper){
-			$classname = "lib_helper_{$helper}";	
-	    $this->{$helper} = & new $classname($this);
-		}
-	}
+        if (!is_scalar($key))
+            return;
 
-	function addscript($a='') {
-		if (in_array($a, $this->scripts))
-			return;	
-		$this->scripts[]=$a;
-	}
+        if (!array_key_exists($key, $_SESSION))
+            return;
 
-	function flash ($key=false) {
-		if (! $key)
-			return;
+        $msg = $_SESSION[$key];
 
-		if (empty($key))
-			return;
+        $_SESSION[$key] = "";
 
-		if (! is_scalar($key))
-			return;
+        return $msg;
+    }
 
-		if (! array_key_exists($key, $_SESSION))
-			return;
+    function render($layout = null, $file = null) {
+        $out = null;
+        $viewoutput = $this->_render("app/views/$file", $this->viewvars);
+        $pageoutput = $this->renderlayout("app/views/layouts/$layout", $viewoutput);
+        $this->hasrendered = true;
+        return $pageoutput;
+    }
 
-		$msg = $_SESSION[$key];
+    function renderlayout($layoutfilename = null, $content) {
+        $data = array_merge(
+            $this->viewvars, array(
+                'content' => $content,
+                'headers' => implode("\t\r\n", $this->scripts)
+            )
+        );
+        return $this->_render($layoutfilename, $data);
+    }
 
-		$_SESSION[$key] = "";
-		//unset ($_SESSION[$key]);
+    function _render($filename, $data) {
+        $filename = $filename . ".php";
+        extract($data, EXTR_SKIP);
+        ob_start();
+        if (!file_exists($filename))
+            echo "filename $filename does not exist";
+        else
+            include ($filename);
+        return ob_get_clean();
+    }
 
-		return $msg;	
-	}
+    function element($elementfilename, $data = array()) {
+        $element = $this->_render("app/views/elements/{$elementfilename}", array_merge($this->viewvars, $data));
+        return $element;
+    }
 
-	function render($layout = null, $file = null) {
-		$out = null;
-		$viewoutput = $this->_render("app/views/$file", $this->viewvars);
-		$pageoutput = $this->renderlayout("app/views/layouts/$layout", $viewoutput);
-		$this->hasrendered = true;
-		return $pageoutput;
-	}
-
-	function renderlayout($layoutfilename = null, $content) {
-		$data = array_merge(
-			$this->viewvars, array (
-				'content' => $content,
-				'headers' => implode ("\t\r\n", $this->scripts)
-			)
-		);
-		return $this->_render($layoutfilename, $data);
-	}
-
-	function _render ($filename, $data) {
-		$filename = $filename.".php";
-   	extract($data, EXTR_SKIP);
-		ob_start();
-		if (!file_exists($filename))
-			echo "filename $filename does not exist";
-		else
-			include ($filename);
-		return ob_get_clean();
-	}	
- 
-	function element ($elementfilename, $data = array()) {
-		$element = $this->_render("app/views/elements/{$elementfilename}", array_merge($this->viewvars, $data));
-		return $element;
-	}
-}
+}?>
